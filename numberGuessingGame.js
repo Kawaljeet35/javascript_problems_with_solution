8f503663384e8a56b2539c73eb03b2fc8d7c77f9
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-13 11:37:51 +0530

    Create numberGuessingGame.js

---
+++ numberGuessingGame.js
@@ -0,0 +1,16 @@
+/*
+Number Guessing Game
+
+Write a program in python which replicates the number guessing game in real life.
+
+The program should generate a random number forehand and prompt the user to 
+guess a number between 1 and 100. Depending upon the user guess, the program should suggest
+the user to either guess a lower or bigger number.
+The program should keep a count on the number of attempts a user takes to guess the 
+correct number.
+
+Example Input/Output:
+
+*/
+
+//Solution:
