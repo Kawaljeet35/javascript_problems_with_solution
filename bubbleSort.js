d1bc40cc44b87ae92d86af501330ef5d349a5bb8
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-23 23:11:38 +0530

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -6,24 +6,21 @@ sort a given list in ascending order.
 */
 
 //Solution:
-function bubbleSort(array)
-{
-  let swapped = false;
+function bubbleSort(array) {
   const length = array.length;
-  for(let i=0; i<length-1; i++){
-    for(let j=0; j<length-1-i; j++){
-      if(array[j]>array[j+1]){
+  for (let i = 0; i < length - 1; i++) {
+    let swapped = false; 
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
         let temp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temp;
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
         swapped = true;
-      } 
-      if(swapped === false){
-        break;
-      }  
+      }
+    }
+    if (swapped === false) {
+      break;
     }  
   }
 }
 
-const a = [8,3,7,4,1];
-console.log(bubbleSort(a));
