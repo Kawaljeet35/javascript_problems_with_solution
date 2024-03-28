235c5e093237648477dad4fdbdb396b38c6a51b1
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-28 15:08:07 +0530

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,25 @@
+/*
+Selection Sort
+
+Write a program in javascript to implement the Selection Sort algorithm and 
+sort a given list in ascending order.
+*/
+
+//Solution:
+function selectionSort(array){
+  for(let i=0; i<array.length-1; i++){
+    let min = i;
+    for(let j=i+1; j<array.length; j++){
+      if(array[j]<array[min]){
+        min = j; 
+      }
+    }
+    if(array[i] !== array[min]){
+      [array[i],array[min]] = [array[min],array[i]];
+    }
+  }
+  return array;
+}
+
+const numbers = [8,3,7,4,1];
+console.log(selectionSort(numbers));
