b571d70ed5725eb90ea5000dc8078548785ad1ae
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-28 15:52:51 +0530

    Create mergeSort.js

---
+++ mergeSort.js
@@ -0,0 +1,44 @@
+/*
+Merge Sort
+
+Write a program in javascript to implement the Merge Sort algorithm and 
+sort a given list in ascending order.
+*/
+
+//Solution:
+function mergeSort(array) {
+  if (array.length <= 1) {
+      return array; 
+  }
+
+  const mid = Math.floor(array.length / 2);
+  const leftArray = array.slice(0, mid);
+  const rightArray = array.slice(mid);
+
+  const sortedLeft = mergeSort(leftArray);
+  const sortedRight = mergeSort(rightArray);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(leftArray, rightArray) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+ 
+  while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+      if (leftArray[leftIndex] < rightArray[rightIndex]) {
+          result.push(leftArray[leftIndex]);
+          leftIndex++;
+      } else {
+          result.push(rightArray[rightIndex]);
+          rightIndex++;
+      }
+  }
+
+  return result.concat(leftArray.slice(leftIndex), rightArray.slice(rightIndex));
+}
+
+const numbers = [8, 3, 7, 4, 1];
+console.log(mergeSort(numbers)); 
