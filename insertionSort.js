0261c209e4e64ddd59af45d944528c0820f7e874
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-28 23:05:37 +0530

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -21,3 +21,11 @@ function insertionSort(array) {
 
 const array = [8, 3, 7, 4, 1];
 console.log(insertionSort(array));
+
+/*
+Time Complexity:
+  Best Case: O(n)
+  Average Case: O(n^2)
+  Worst Case: O(n^2)
+Space Complexity: O(1)
+*/
