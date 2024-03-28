ddbc4b40e8d2799a0d8ef240cc307d88c792ab0f
Kawaljeet Singh <123053797+Kawaljeet35@users.noreply.github.com>
2024-03-28 15:10:26 +0530

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -12,9 +12,7 @@ function bubbleSort(array) {
     let swapped = false; 
     for (let j = 0; j < length - 1 - i; j++) {
       if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+        [ array[j], array[j + 1] ] = [ array[j + 1], array[j] ];
         swapped = true;
       }
     }
